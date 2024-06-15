61278de1cbd764cd862be32b9465a969bca2795e
kennyjayona <kennyjayona7@gmail.com>
2024-06-15 09:27:05 +0800

    first commit

---
+++ causes.php
@@ -0,0 +1,18 @@
+<?php require_once('config.php'); ?>
+<!DOCTYPE HTML>
+<head>
+<title> Causes | <?php echo $_settings->info('short_name') ?></title>
+<meta name="description" content="">
+<meta name="keywords" content="Charity Causes, Sample Cause, My Sample Charity Website">
+<meta name="robots" content="index, follow">
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Charity Causes" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="http://localhost/charity/uploads/1629161280_charity_logo.jpg" />
+<meta property="og:url" content="http://localhost/charity/causes.php" />
+<?php require_once(base_app.'inc/page_header.php') ?>
+</head>
+<?php include(base_app.'inc/body_block.php') ?>
+
+<p style="text-align: center; "><span style="font-size: 36px;">Causes</span></p>
+<?php include(base_app.'inc/body_block_end.php') ?>
---
+++ pages/sample_102.php
@@ -0,0 +1,22 @@
+<?php require_once('../config.php'); ?>
+<!DOCTYPE HTML>
+<head>
+<title> Sample 102 | <?php echo $_settings->info('short_name') ?></title>
+<meta name="description" content="Nullam pulvinar libero id justo faucibus elementum. Donec euismod ante tellus, et pulvinar ipsum tincidunt ac. Proin efficitur eros eu orci imperdiet fringilla. Sed ullamcorper luctus lacus, in varius sapien tincidunt gravida">
+<meta name="keywords" content="Sample, Content, Sample 102, keyword">
+<meta name="robots" content="index, follow">
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Sample 102" />
+<meta property="og:description" content="Nullam pulvinar libero id justo faucibus elementum. Donec euismod ante tellus, et pulvinar ipsum tincidunt ac. Proin efficitur eros eu orci imperdiet fringilla. Sed ullamcorper luctus lacus, in varius sapien tincidunt gravida" />
+<meta property="og:image" content="http://localhost/charity/uploads/blog_uploads/banners/2_banner.jpg" />
+<meta property="og:url" content="http://localhost/charity/pages/sample_102.php" />
+<?php require_once('../inc/page_header.php') ?>
+</head>
+<?php include(base_app.'inc/body_block.php') ?>
+<h2>Sample 102</h2>
+<input name="blog_id" value="c81e728d9d4c2f636f067f89cc14862c" type="hidden">
+<hr>
+<span class='text-muted'><i class='fa fa-calendar-day'></i> Published: Aug 17,2021 01:57 PM</span>
+<center><img src="http://localhost/charity/uploads/blog_uploads/banners/2_banner.jpg" class="img-thumbnail img-banner" alt="http://localhost/charity/pages/sample_102.php" /></center>
+<h3 style="text-align: center; margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;"><p style="color: rgb(33, 37, 41); font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-size: 16px; text-align: left; background-color: rgb(255, 255, 255);"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">The pain itself is love, the main storage system. </font><font style="vertical-align: inherit;">Even the mass of the earth bears any life at the author's mi. </font><font style="vertical-align: inherit;">The grid is no longer available to the zenith of the archers, the members of the pure wise just, nor the mass of the mass needs before the ends. </font><font style="vertical-align: inherit;">Maecenas vitae eros in purus dictum porttitor. </font><font style="vertical-align: inherit;">The entire bow is worth it, and the earth is said and, it is just a sauce. </font><font style="vertical-align: inherit;">Maecenas rutrum erat tincidunt dui makeup fermentum. </font><font style="vertical-align: inherit;">No cost annoying child. </font><font style="vertical-align: inherit;">But my just, that's just the gate and, to decorate it with some. </font><font style="vertical-align: inherit;">Cras porta nisi eu eleifend tincidunt. </font><font style="vertical-align: inherit;">Unfortunately, sometimes the average person is a lot of trouble. </font><font style="vertical-align: inherit;">Maecenas sed augue condimentum justo vulputate interdum vel in lacus.</font></font><br></p><div><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><br></font></font></div></h3>
+<?php include(base_app.'inc/body_block_end.php') ?>
